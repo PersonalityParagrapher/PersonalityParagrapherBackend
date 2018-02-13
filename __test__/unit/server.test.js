5adbb95154244a14084d262d3b4de17d60ff8a39
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-12 21:27:03 -0800

    Fixed a typo

--- __test__/unit/server.test.js
+++ __test__/unit/server.test.js
@@ -7,7 +7,7 @@ describe('Server Module', function() {
   beforeAll(server.start);
   afterAll(server.stop);
 
-  it('should return a promise rejction if the server is already running when started', () => {
+  it('should return a promise rejection if the server is already running when started', () => {
     server.start()
       .catch(err => expect(err.message).toMatch(/Server running/i));
   });
