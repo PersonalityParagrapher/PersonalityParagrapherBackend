9a73526c32de8e8bdccb84583aa723acb3cffec8
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-12 22:03:18 -0800

    Fixed some jest errors

--- route/route-insight-song.js
+++ route/route-insight-song.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Comment out for now to stop jest from complaining
-// module.exports = function(router) {
-//   console.log(router);
-// };
+ module.exports = function(router) {
+  router;
+ };
